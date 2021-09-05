2075162cde5b835b485222db9ed2426a9d521179
Tasnim El harasse <6361856-tasnim_elharasse@users.noreply.gitlab.com>
2021-09-05 11:25:08 +0200

    first commit

---
+++ python-lec1.py
@@ -0,0 +1,77 @@
+#Number data types
+#integer
+var1 =5
+#float
+var2=4.3
+
+#String
+var3="hello world!!"
+#Character
+var4="H"
+
+#convert from string to int
+txt="4"
+sum = 9+int(txt)
+
+#Boolean
+boolean =True
+
+#Array
+arr = ['one',2,7,'python']
+#printing first value
+print(arr[0])
+
+
+#Dictionary
+dict ={
+    'name' :'tasnim',
+    'age' :24,
+    'email' :'tasnim@gmail,com'
+    }
+#printing first value
+print(arr['age'])
+
+#Array of Dictionaries
+students =[
+    {
+    'name' :'tasnim',
+    'age' :24,
+    'email' :'tasnim@gmail,com'
+    },
+    {
+    'name' :'alaa',
+    'age' :20,
+    'email' :'alaa@gmail,com'
+    },
+    {
+    'name' :'abdullah',
+    'age' :22,
+    'email' :'abdullah@gmail,com'
+    }
+]
+
+#print second value of array and name key of dictionary
+print(students[1]['name'])
+
+#Deleting a variable
+var_int =9+5
+print(var_int)
+del var_int
+print(var_int)
+
+#Constants
+PI =3.14
+GRAVITY =9.8
+rad = 5
+area= 3.14*5*5
+PI=7
+print(PI)
+
+#Inputing values from users
+var_num = input("Please enter a value")
+sum = int(var_num) +8
+print("Your sum is",sum)
+
+#importing a file 
+#import filename 
+
